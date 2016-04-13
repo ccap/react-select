bc069c1a23472d0c8870f922699111e718b6fadc
Peter Brant <peter.brant@gmail.com>
2016-04-13 19:06:15 +0200

    Generate uniqueId in componentWillMount()

--- src/Select.js
+++ src/Select.js
@@ -152,6 +152,11 @@ const Select = React.createClass({
 		};
 	},
 
+	componentWillMount() {
+		this.descriptionUniqueId = uniqueId("Select_input_description");
+		this.listUniqueId = uniqueId("Select_listbox");
+	},
+
 	componentDidMount () {
 		if (this.props.autofocus) {
 			this.focus();
@@ -620,7 +625,7 @@ const Select = React.createClass({
 		}
 	},
 
-	renderInput (valueArray, descriptionUniqueId, listUniqueId) {
+	renderInput (valueArray) {
 		var className = classNames('Select-input', this.props.inputProps.className);
 		if (this.props.disabled || !this.props.searchable) {
 			return (
@@ -649,7 +654,7 @@ const Select = React.createClass({
 					value={this.state.inputValue}
 					role="combobox"
 					aria-autocomplete="inline"
-					aria-describedby={descriptionUniqueId}
+					aria-describedby={this.descriptionUniqueId}
 					aria-required={this.props.ariaRequired}
 				/>
 			);
@@ -667,7 +672,7 @@ const Select = React.createClass({
 					value={this.state.inputValue}
 					role="combobox"
 					aria-autocomplete="inline"
-					aria-describedby={descriptionUniqueId}
+					aria-describedby={this.descriptionUniqueId}
 					aria-required={this.props.ariaRequired}
 				/>
 			</div>
@@ -824,7 +829,7 @@ const Select = React.createClass({
 		}
 	},
 
-	renderOuter (options, valueArray, focusedOption, listUniqueId) {
+	renderOuter (options, valueArray, focusedOption) {
 		let menu = this.renderMenu(options, valueArray, focusedOption);
 		if (!menu) {
 			return null;
@@ -834,7 +839,7 @@ const Select = React.createClass({
 			<div ref="menuContainer" className="Select-menu-outer" style={this.props.menuContainerStyle}>
 				<div ref="menu" className="Select-menu"
 						 role="listbox"
-						 id={listUniqueId}
+						 id={this.listUniqueId}
 						 style={this.props.menuStyle}
 						 onScroll={this.handleMenuScroll}
 						 onMouseDown={this.handleMouseDownOnMenu}>
@@ -844,14 +849,14 @@ const Select = React.createClass({
 		);
 	},
 
-	renderInputDescription (valueArray, descriptionUniqueId) {
+	renderInputDescription (valueArray) {
 		if (this.state.focusedOption && this.state.isOpen) {
-			return <div className="Select-sr-only" aria-hidden="true" id={descriptionUniqueId}>
+			return <div className="Select-sr-only" aria-hidden="true" id={this.descriptionUniqueId}>
 				Selected option: {this.getOptionLabel(this.state.focusedOption)}
 			</div>;
 		} else {
 			const renderLabel = this.props.valueRenderer || this.getOptionLabel;
-			return <div className="Select-sr-only" aria-hidden="true" id={descriptionUniqueId}>
+			return <div className="Select-sr-only" aria-hidden="true" id={this.descriptionUniqueId}>
 				{ valueArray.length ?
 					<span>Current value: {valueArray.map(v => renderLabel(v))}</span> :
 					this.props.placeholder
@@ -877,13 +882,10 @@ const Select = React.createClass({
 			'has-value': valueArray.length,
 		});
 
-		const descriptionUniqueId = uniqueId("Select_input_description");
-		const listUniqueId = uniqueId("Select_listbox");
-
 		return (
 			<div ref="wrapper" className={className} style={this.props.wrapperStyle}>
 				{this.renderHiddenField(valueArray)}
-				{this.renderInputDescription(valueArray, descriptionUniqueId)}
+				{this.renderInputDescription(valueArray)}
 				<div ref="control"
 						 className="Select-control"
 						 style={this.props.style}
@@ -893,12 +895,12 @@ const Select = React.createClass({
 						 onTouchStart={this.handleTouchStart}
 						 onTouchMove={this.handleTouchMove}>
 					{this.renderValue(valueArray, isOpen)}
-					{this.renderInput(valueArray, descriptionUniqueId, listUniqueId)}
+					{this.renderInput(valueArray)}
 					{this.renderLoading()}
 					{this.renderClear()}
 					{this.renderArrow()}
 				</div>
-				{isOpen ? this.renderOuter(options, !this.props.multi ? valueArray : null, focusedOption, listUniqueId) : null}
+				{isOpen ? this.renderOuter(options, !this.props.multi ? valueArray : null, focusedOption) : null}
 			</div>
 		);
 	}
